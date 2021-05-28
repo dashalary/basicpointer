94b0e24a2a1dd3495d8c4b32dac187a10844a03d
Dasha Lary <dasha4988@gmail.com>
2021-05-28 16:30:03 -0400

    built and ran code

--- main.c
+++ main.c
@@ -3,5 +3,15 @@
 
 int main()
 {
+    int number = 150;
+    int *pNumber = NULL;
+
+    pNumber = &number;
+
+    printf("number address is: %p\n", &number);
+    printf("address of pNumber: %p\n", &pNumber);
+    printf("value of pNumber: %p\n", pNumber);
+    printf("value of what pNumber is pointing to: %d\n", *pNumber);
+
     return 0;
 }
